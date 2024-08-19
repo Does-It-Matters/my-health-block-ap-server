0d770f5309ef1284c07790ec4288f702ab6808a2
glass <silver20190204@gmail.com>
2024-08-20 01:12:36 +0900

    Add doctor tests

---
+++ src/test/java/com/example/myhealthblock/doctor/adapter/in/web/response/SignUpResultResponseTest.java
@@ -0,0 +1,17 @@
+package com.example.myhealthblock.doctor.adapter.in.web.response;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SignUpResultResponseTest {
+
+    @Test
+    public void testGettersAndSetters() {
+        SignUpResultResponse response = new SignUpResultResponse("success");
+
+        assertEquals("success", response.getResult());
+
+        response.setResult("failure");
+        assertEquals("failure", response.getResult());
+    }
+}
