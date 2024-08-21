737b87f59731ad93b22f6a323d89bcd4d74a8a92
glass <silver20190204@gmail.com>
2024-08-21 14:50:27 +0900

    JavaDoc 추가

--- src/main/java/com/example/myhealthblock/user/domain/model/User.java
+++ src/main/java/com/example/myhealthblock/user/domain/model/User.java
@@ -21,11 +21,28 @@ public class User implements SignInable, PasswordManager {
     String pw;
     String role;
 
+    /**
+     * <b> 역할: 로그인 메소드 </b>
+     * <p>
+     * - 비밀번호가 동일한지 확인 <br>
+     *
+     * @return 비밀번호와 동일하면 {@code true}, 동일하지 않으면 {@code false} <br>
+     * </p>
+     */
     @Override
     public boolean signIn(String pw) {
         return this.pw.equals(pw);
     }
 
+    /**
+     * <b> 역할: 비밀번호 변경 메소드 </b>
+     * <p>
+     * - 기존 비밀번호와 동일하면 새로운 비밀번호로 변경 <br>
+     * - 입력 유효성 검사 로직 필요 <br>
+     *
+     * @return 변경되면 {@code true}, 변경되지 않으면 {@code false} <br>
+     * </p>
+     */
     @Override
     public boolean changePw(String oldPw, String newPw) {
         if (pw.equals(oldPw)) {
