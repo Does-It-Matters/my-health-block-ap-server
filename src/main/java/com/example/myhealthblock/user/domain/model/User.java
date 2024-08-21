433eb934f3371e508d0057c9b0ffb59e0ad49357
glass <silver20190204@gmail.com>
2024-08-21 15:20:13 +0900

    JavaDoc 추가

--- src/main/java/com/example/myhealthblock/user/domain/model/User.java
+++ src/main/java/com/example/myhealthblock/user/domain/model/User.java
@@ -26,6 +26,7 @@ public class User implements SignInable, PasswordManager {
      * <p>
      * - 비밀번호가 동일한지 확인 <br>
      *
+     * @param pw 사용자가 로그인 요청 시 입력한 비밀번호
      * @return 비밀번호와 동일하면 {@code true}, 동일하지 않으면 {@code false} <br>
      * </p>
      */
@@ -40,6 +41,8 @@ public class User implements SignInable, PasswordManager {
      * - 기존 비밀번호와 동일하면 새로운 비밀번호로 변경 <br>
      * - 입력 유효성 검사 로직 필요 <br>
      *
+     * @param oldPw 사용자가 비밀번호 변경을 위해 입력한 기존 비밀번호
+     * @param newPw 사용자가 비밀번호 변경을 위해 입력한 변경 비밀번호
      * @return 변경되면 {@code true}, 변경되지 않으면 {@code false} <br>
      * </p>
      */
