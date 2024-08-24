ccc7c7e39af325873e13e61d10ced331cc074f27
glass <silver20190204@gmail.com>
2024-08-25 00:38:13 +0900

    Add Patient 입력 어댑터, 테스트

--- src/main/java/com/example/myhealthblock/patient/application/port/in/dto/PatientSignUpInputPortRequest.java
+++ src/main/java/com/example/myhealthblock/patient/application/port/in/dto/PatientSignUpInputPortRequest.java
@@ -1,10 +1,14 @@
 package com.example.myhealthblock.patient.application.port.in.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientSignUpInputPortRequest {
     String id;
     String pw;
