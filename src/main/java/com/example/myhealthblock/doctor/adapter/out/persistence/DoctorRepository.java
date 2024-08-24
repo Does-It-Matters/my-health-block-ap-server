edecf086eb00b7b7ef870dbf16adbec692485242
glass <silver20190204@gmail.com>
2024-08-24 21:53:37 +0900

    Add Doctor 어댑터

--- src/main/java/com/example/myhealthblock/doctor/adapter/out/persistence/DoctorRepository.java
+++ src/main/java/com/example/myhealthblock/doctor/adapter/out/persistence/DoctorRepository.java
@@ -1,18 +1,18 @@
-//package com.example.myhealthblock.doctor.adapter.out.persistence;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-///**
-// * <b> 역할: 의료진 데이터에 대한 리포지토리 인터페이스 </b>
-// * <br>- 의료진 리포지토리
-// */
-//public interface DoctorRepository extends JpaRepository<DoctorEntity, Integer> {
-//
-//    /**
-//     * <b> 역할: 사용자 ID로 의료진 엔티티 조회 메소드 </b>
-//     *
-//     * @param userId 의료진 ID
-//     * @return 의료진 엔티티
-//     */
-//    DoctorEntity findByUserId(String userId);
-//}
+package com.example.myhealthblock.doctor.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * <b> 역할: 의료진 데이터에 대한 리포지토리 인터페이스 </b>
+ * <br>- 의료진 리포지토리
+ */
+public interface DoctorRepository extends JpaRepository<DoctorEntity, Integer> {
+
+    /**
+     * <b> 역할: 사용자 ID로 의료진 엔티티 조회 메소드 </b>
+     *
+     * @param userId 의료진 ID
+     * @return 의료진 엔티티
+     */
+    DoctorEntity findByUserId(String userId);
+}
