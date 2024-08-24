ed6971b682c8b39399f6d002a4c5ed80167bb7d8
glass <silver20190204@gmail.com>
2024-08-25 00:53:50 +0900

    Add Question 출력 포트

--- src/main/java/com/example/myhealthblock/question/application/service/QuestionService.java
+++ src/main/java/com/example/myhealthblock/question/application/service/QuestionService.java
@@ -1,59 +1,59 @@
-//package com.example.myhealthblock.question.application.service;
-//
-//import com.example.myhealthblock.question.application.port.in.QuestionInport;
-//import com.example.myhealthblock.question.application.port.in.dto.QuestionEnrollInportRequest;
-//import com.example.myhealthblock.question.application.port.out.dto.QuestionEnrollOutportRequest;
-//import com.example.myhealthblock.question.common.Category;
-//import com.example.myhealthblock.question.application.port.out.QuestionOutport;
-//import com.example.myhealthblock.question.domain.dto.QuestionDTO;
-//import com.example.myhealthblock.question.domain.dto.QuestionTitleDTO;
-//import com.example.myhealthblock.question.domain.mapper.QuestionMapper;
-//import org.springframework.stereotype.Service;
-//import lombok.RequiredArgsConstructor;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class QuestionService implements QuestionInport {
-//    private final QuestionOutport outport;
-//    private final QuestionMapper mapper = QuestionMapper.INSTANCE;
-//
-//    @Override
-//    public String enroll(QuestionEnrollInportRequest dto) {
-//        QuestionEnrollOutportRequest request = mapper.INSTANCE.inportRequestToOutportRequest(dto);
-//        boolean result = outport.create(request);
-//
-//        return result ? "success" : "fail";
-//    }
-//
-//    @Override
-//    public QuestionDTO getQuestion(Integer questionId) {
-//        return outport.getQuestion(questionId);
-//    }
-//
-//    @Override
-//    public QuestionTitleDTO[] getQuestions(int userId) {
-//        return outport.getQuestions(userId);
-//    }
-//
-//    @Override
-//    public QuestionTitleDTO[] getQuestions(Category category) {
-//        return outport.getQuestions(category);
-//    }
-//
-//    @Override
-//    public QuestionTitleDTO[] getQuestions() {
-//        return outport.getQuestions();
-//    }
-//
-//    @Override
-//    public String update(Integer questionId, String title, String symptom, String content) {
-//        boolean result = outport.update(questionId, title, symptom, content);
-//        return result ? "success" : "fail";
-//    }
-//
-//    @Override
-//    public String delete(Integer questionId) {
-//        boolean result = outport.delete(questionId);
-//        return result ? "success" : "fail";
-//    }
-//}
+package com.example.myhealthblock.question.application.service;
+
+import com.example.myhealthblock.question.application.port.in.QuestionInport;
+import com.example.myhealthblock.question.application.port.in.dto.QuestionEnrollInportRequest;
+import com.example.myhealthblock.question.application.port.out.QuestionOutputPort;
+import com.example.myhealthblock.question.application.port.out.dto.QuestionEnrollOutportRequest;
+import com.example.myhealthblock.question.common.Category;
+import com.example.myhealthblock.question.domain.dto.QuestionDTO;
+import com.example.myhealthblock.question.domain.dto.QuestionTitleDTO;
+import com.example.myhealthblock.question.domain.mapper.QuestionMapper;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService implements QuestionInport {
+    private final QuestionOutputPort outputPort;
+    private final QuestionMapper mapper = QuestionMapper.INSTANCE;
+
+    @Override
+    public String enroll(QuestionEnrollInportRequest dto) {
+        QuestionEnrollOutportRequest request = mapper.INSTANCE.inportRequestToOutportRequest(dto);
+        boolean result = outputPort.create(request);
+
+        return result ? "success" : "fail";
+    }
+
+    @Override
+    public QuestionDTO getQuestion(Integer questionId) {
+        return outputPort.getQuestion(questionId);
+    }
+
+    @Override
+    public QuestionTitleDTO[] getQuestions(int userId) {
+        return outputPort.getQuestions(userId);
+    }
+
+    @Override
+    public QuestionTitleDTO[] getQuestions(Category category) {
+        return outputPort.getQuestions(category);
+    }
+
+    @Override
+    public QuestionTitleDTO[] getQuestions() {
+        return outputPort.getQuestions();
+    }
+
+    @Override
+    public String update(Integer questionId, String title, String symptom, String content) {
+        boolean result = outputPort.update(questionId, title, symptom, content);
+        return result ? "success" : "fail";
+    }
+
+    @Override
+    public String delete(Integer questionId) {
+        boolean result = outputPort.delete(questionId);
+        return result ? "success" : "fail";
+    }
+}
--- src/main/java/com/example/myhealthblock/question/domain/mapper/QuestionMapper.java
+++ src/main/java/com/example/myhealthblock/question/domain/mapper/QuestionMapper.java
@@ -1,11 +1,11 @@
-//package com.example.myhealthblock.question.domain.mapper;
-//
-//import com.example.myhealthblock.question.application.port.in.dto.QuestionEnrollInportRequest;
-//import com.example.myhealthblock.question.application.port.out.dto.QuestionEnrollOutportRequest;
-//import org.mapstruct.factory.Mappers;
-//
-//public interface QuestionMapper {
-//    QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
-//
-//    QuestionEnrollOutportRequest inportRequestToOutportRequest(QuestionEnrollInportRequest inportRequest);
-//}
+package com.example.myhealthblock.question.domain.mapper;
+
+import com.example.myhealthblock.question.application.port.in.dto.QuestionEnrollInportRequest;
+import com.example.myhealthblock.question.application.port.out.dto.QuestionEnrollOutportRequest;
+import org.mapstruct.factory.Mappers;
+
+public interface QuestionMapper {
+    QuestionMapper INSTANCE = Mappers.getMapper(QuestionMapper.class);
+
+    QuestionEnrollOutportRequest inportRequestToOutportRequest(QuestionEnrollInportRequest inportRequest);
+}
