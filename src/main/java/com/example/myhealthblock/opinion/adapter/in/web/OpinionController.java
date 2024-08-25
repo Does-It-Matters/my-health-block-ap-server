ea5c29b85459b78407347b19bdcf25c2b57eaa1e
glass <silver20190204@gmail.com>
2024-08-25 09:40:29 +0900

    Add Opinion 출력 어댑터

--- src/main/java/com/example/myhealthblock/opinion/adapter/in/web/OpinionController.java
+++ src/main/java/com/example/myhealthblock/opinion/adapter/in/web/OpinionController.java
@@ -1,87 +1,87 @@
-//package com.example.myhealthblock.opinion.adapter.in.web;
-//
-//import com.example.myhealthblock.aop.LogExecutionTime;
-//import com.example.myhealthblock.aop.LogTarget;
-//import com.example.myhealthblock.opinion.application.port.in.OpinionInport;
-//import com.example.myhealthblock.opinion.adapter.in.web.request.OpinionEnrollRequest;
-//import com.example.myhealthblock.opinion.adapter.in.web.response.EnrollOpinionResponse;
-//import com.example.myhealthblock.opinion.adapter.in.web.response.OpinionsResponse;
-//import com.example.myhealthblock.opinion.adapter.in.web.response.ResultResponse;
-//import com.example.myhealthblock.opinion.application.port.in.dto.OpinionInportDTO;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-///**
-// * <b> 역할: 의견 관련 컨트롤러 </b>
-// * <br>- 의견 관리
-// */
-//@Tag(name = "Opinion", description = "Endpoints for opinion")
-//@LogExecutionTime(logTarget = LogTarget.CONTROLLER)
-//@RequiredArgsConstructor
-//@RestController
-//@RequestMapping("/api")
-//public class OpinionController {
-//    private final OpinionInport opinionInport;
-//
-//    /**
-//     * <b> 역할: 의견 등록 메소드 </b>
-//     *
-//     * @param body 의견 등록 HTTP 요청 바디 데이터
-//     * @return 의견 등록 응답
-//     */
-//    @Operation(summary = "의견 등록", description = "유저가 의견 등록")
-//    @PostMapping("/v2/opinion/enroll")
-//    public ResponseEntity<EnrollOpinionResponse> enroll(@RequestBody OpinionEnrollRequest body) {
-//        OpinionInportDTO dto = opinionInport.enroll(body.toInportDTO());
-//        return ResponseEntity.ok(new EnrollOpinionResponse(dto));
-//    }
-//
-//    /**
-//     * <b> 역할: 여러 의견 조회 메소드 </b>
-//     *
-//     * @param questionId 질문 식별자
-//     * @param userId 회원가입 시 등록한 아이디
-//     * @return 여러 의견
-//     */
-//    @Operation(summary = "의견들 조회", description = "유저가 의견들 조회 <br>questionId는 질문 식별자로, 입력 시 특정 질문에 대한 의견들 조회 <br>userId는 회원가입 시 등록한 아이디로, 입력 시 본인이 등록한 의견들 조회<br>둘 중 하나만 입력")
-//    @GetMapping("/v2/opinions")
-//    public ResponseEntity<OpinionsResponse> getOpinions(@RequestParam(required = false) Integer questionId, @RequestParam(required = false) Integer userId) {
-//        OpinionInportDTO[] list = getOpinionDTOs(questionId, userId);
-//        return ResponseEntity.ok(new OpinionsResponse(list));
-//    }
-//
-//    /**
-//     * <b> 역할: 상황에 따른 의견 조회 메소드 </b>
-//     * - userId가 있으면 해당 사용자 의견 조회
-//     * - questionId가 있으면 해당 질문에 대한 의견 조회
-//     *
-//     * @param questionId 질문 식별자
-//     * @param userId 회원가입 시 등록한 아이디
-//     * @return 여러 의견
-//     */
-//    private OpinionInportDTO[] getOpinionDTOs(Integer questionId, Integer userId) {
-//        OpinionInportDTO[] list = null;
-//        if (userId != null) {
-//            list = opinionInport.getOpinionsByUserId(userId);
-//        } else if (questionId != null){
-//            list = opinionInport.getOpinionsByQuestionId(questionId);
-//        }
-//        return list;
-//    }
-//
-//    /**
-//     * <b> 역할: 의견 삭제 메소드 </b>
-//     *
-//     * @param opinionId 삭제할 의견 아이디
-//     * @return 회원가입 응답
-//     */
-//    @Operation(summary = "의견 삭제", description = "유저가 의견 삭제 <br>{opinionId}는 식별자")
-//    @DeleteMapping("/v2/opinion/{opinionId}")
-//    public ResponseEntity<ResultResponse> delete(@PathVariable Integer opinionId) {
-//        String result = opinionInport.delete(opinionId);
-//        return ResponseEntity.ok(new ResultResponse(result));
-//    }
-//}
+package com.example.myhealthblock.opinion.adapter.in.web;
+
+import com.example.myhealthblock.aop.LogExecutionTime;
+import com.example.myhealthblock.aop.LogTarget;
+import com.example.myhealthblock.opinion.adapter.in.web.request.OpinionEnrollRequest;
+import com.example.myhealthblock.opinion.adapter.in.web.response.EnrollOpinionResponse;
+import com.example.myhealthblock.opinion.adapter.in.web.response.OpinionsResponse;
+import com.example.myhealthblock.opinion.adapter.in.web.response.ResultResponse;
+import com.example.myhealthblock.opinion.application.port.in.OpinionInputPort;
+import com.example.myhealthblock.opinion.application.port.in.dto.OpinionInputPortDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <b> 역할: 의견 관련 컨트롤러 </b>
+ * <br>- 의견 관리
+ */
+@Tag(name = "Opinion", description = "Endpoints for opinion")
+@LogExecutionTime(logTarget = LogTarget.CONTROLLER)
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class OpinionController {
+    private final OpinionInputPort opinionInputPort;
+
+    /**
+     * <b> 역할: 의견 등록 메소드 </b>
+     *
+     * @param body 의견 등록 HTTP 요청 바디 데이터
+     * @return 의견 등록 응답
+     */
+    @Operation(summary = "의견 등록", description = "유저가 의견 등록")
+    @PostMapping("/v2/opinion/enroll")
+    public ResponseEntity<EnrollOpinionResponse> enroll(@RequestBody OpinionEnrollRequest body) {
+        OpinionInputPortDTO dto = opinionInputPort.enroll(body.toInputPortDTO());
+        return ResponseEntity.ok(new EnrollOpinionResponse(dto));
+    }
+
+    /**
+     * <b> 역할: 여러 의견 조회 메소드 </b>
+     *
+     * @param questionId 질문 식별자
+     * @param userId 회원가입 시 등록한 아이디
+     * @return 여러 의견
+     */
+    @Operation(summary = "의견들 조회", description = "유저가 의견들 조회 <br>questionId는 질문 식별자로, 입력 시 특정 질문에 대한 의견들 조회 <br>userId는 회원가입 시 등록한 아이디로, 입력 시 본인이 등록한 의견들 조회<br>둘 중 하나만 입력")
+    @GetMapping("/v2/opinions")
+    public ResponseEntity<OpinionsResponse> getOpinions(@RequestParam(required = false) Integer questionId, @RequestParam(required = false) Integer userId) {
+        OpinionInputPortDTO[] list = getOpinionDTOs(questionId, userId);
+        return ResponseEntity.ok(new OpinionsResponse(list));
+    }
+
+    /**
+     * <b> 역할: 상황에 따른 의견 조회 메소드 </b>
+     * - userId가 있으면 해당 사용자 의견 조회
+     * - questionId가 있으면 해당 질문에 대한 의견 조회
+     *
+     * @param questionId 질문 식별자
+     * @param userId 회원가입 시 등록한 아이디
+     * @return 여러 의견
+     */
+    private OpinionInputPortDTO[] getOpinionDTOs(Integer questionId, Integer userId) {
+        OpinionInputPortDTO[] list = null;
+        if (userId != null) {
+            list = opinionInputPort.getOpinionsByUserId(userId);
+        } else if (questionId != null){
+            list = opinionInputPort.getOpinionsByQuestionId(questionId);
+        }
+        return list;
+    }
+
+    /**
+     * <b> 역할: 의견 삭제 메소드 </b>
+     *
+     * @param opinionId 삭제할 의견 아이디
+     * @return 회원가입 응답
+     */
+    @Operation(summary = "의견 삭제", description = "유저가 의견 삭제 <br>{opinionId}는 식별자")
+    @DeleteMapping("/v2/opinion/{opinionId}")
+    public ResponseEntity<ResultResponse> delete(@PathVariable Integer opinionId) {
+        String result = opinionInputPort.delete(opinionId);
+        return ResponseEntity.ok(new ResultResponse(result));
+    }
+}
--- src/main/java/com/example/myhealthblock/opinion/adapter/in/web/request/OpinionEnrollRequest.java
+++ src/main/java/com/example/myhealthblock/opinion/adapter/in/web/request/OpinionEnrollRequest.java
@@ -1,29 +1,29 @@
-//package com.example.myhealthblock.opinion.adapter.in.web.request;
-//
-//import com.example.myhealthblock.opinion.application.port.in.dto.OpinionEnrollInportRequest;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class OpinionEnrollRequest {
-//    int userId;
-//    String content;
-//    Integer questionId;
-//
-//    /**
-//     * <b> 역할: 도메인 계층으로 전달하기 위해 입력 포트에서 전달되는 DTO를 매핑하는 메소드 </b>
-//     * @return 입력 포트에서 전달되는 DTO
-//     */
-//    public OpinionEnrollInportRequest toInportDTO() {
-//        return new OpinionEnrollInportRequest(
-//                userId,
-//                content,
-//                questionId
-//        );
-//    }
-//}
+package com.example.myhealthblock.opinion.adapter.in.web.request;
+
+import com.example.myhealthblock.opinion.application.port.in.dto.OpinionEnrollInputPortRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OpinionEnrollRequest {
+    int userId;
+    String content;
+    Integer questionId;
+
+    /**
+     * <b> 역할: 도메인 계층으로 전달하기 위해 입력 포트에서 전달되는 DTO를 매핑하는 메소드 </b>
+     * @return 입력 포트에서 전달되는 DTO
+     */
+    public OpinionEnrollInputPortRequest toInputPortDTO() {
+        return new OpinionEnrollInputPortRequest(
+                userId,
+                content,
+                questionId
+        );
+    }
+}
