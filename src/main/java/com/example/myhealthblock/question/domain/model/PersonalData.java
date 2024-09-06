f2422330b15be163b3fe30bb864a45bbc09cdcb8
young_ogi <duddhrdl2727@naver.com>
2024-09-06 16:59:16 +0900

    Add Question JavaDoc 추가 my-health-block-ap-server #125

--- src/main/java/com/example/myhealthblock/question/domain/model/PersonalData.java
+++ src/main/java/com/example/myhealthblock/question/domain/model/PersonalData.java
@@ -4,6 +4,9 @@ import com.example.myhealthblock.question.common.Gender;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * <b> 역할: PersonalData 도메인 모델 클래스 </b>
+ */
 @Getter
 @Setter
 public class PersonalData {
