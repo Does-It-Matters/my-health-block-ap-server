038c055f67843d8a048d6d220e31af50fcf545d9
young_ogi <duddhrdl2727@naver.com>
2024-09-08 23:03:42 +0900

    Add Question JavaDoc 추가 my-health-block-ap-server #125

--- src/main/java/com/example/myhealthblock/question/adapter/out/jpa/question/QuestionEntity.java
+++ src/main/java/com/example/myhealthblock/question/adapter/out/jpa/question/QuestionEntity.java
@@ -13,12 +13,29 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <b>역할: 사용자의 질문 정보를 나타내는 엔티티 클래스</b>
+ * <p>
+ * - 데이터베이스의 'Question' 테이블과 매핑 <br>
+ * - 사용자 질문의 상세 정보(제목, 카테고리, 증상, 내용 등)를 저장 <br>
+ * </p>
+ */
 @Getter
 @Setter
 @Entity(name = "Question")
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class QuestionEntity {
+
+    /**
+     * 모든 필수 필드를 포함하는 생성자
+     *
+     * @param userId 사용자 ID
+     * @param title 질문 제목
+     * @param category 질문 카테고리
+     * @param symptom 증상 설명
+     * @param content 질문 내용
+     */
     public QuestionEntity(int userId, String title, Category category, String symptom, String content){
         this.userId = userId;
         this.title = title;
