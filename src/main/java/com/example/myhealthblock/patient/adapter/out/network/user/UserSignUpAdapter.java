3ca10319d6f9311c65f48dd3642d5f7b09ab9c92
glass <silver20190204@gmail.com>
2024-09-13 00:39:34 +0900

    Add Patient sign up adapter (Issue #136)

---
+++ src/main/java/com/example/myhealthblock/patient/adapter/out/network/user/UserSignUpAdapter.java
@@ -0,0 +1,35 @@
+package com.example.myhealthblock.patient.adapter.out.network.user;
+
+import com.example.myhealthblock.patient.application.port.out.UserSignUpOutputPort;
+import com.example.myhealthblock.patient.application.port.out.dto.PatientSignUpOutputPortToUserRequest;
+import com.example.myhealthblock.user.application.port.in.UserSignUp;
+import com.example.myhealthblock.user.application.port.in.dto.UserSignUpInputPortRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserSignUpAdapter implements UserSignUpOutputPort {
+    private final UserSignUp userInputPort;
+
+    /**
+     * <b> 역할: 사용자 생성 요청 메소드 </b>
+     * <p>
+     *
+     * @param dto 사용자 생성에 필요한 정보를 가진 dto
+     * @return 사용자가 성공적으로 생성되면 {@code true}, 그렇지 않으면 {@code false}
+     * </p>
+     */
+    @Override
+    public boolean signUp(PatientSignUpOutputPortToUserRequest dto) {
+        return userInputPort.signUp(getRequest(dto));
+    }
+
+    private UserSignUpInputPortRequest getRequest(PatientSignUpOutputPortToUserRequest dto) {
+        UserSignUpInputPortRequest request = new UserSignUpInputPortRequest();
+        request.setId(dto.getId());
+        request.setPw(dto.getPw());
+        request.setRole(dto.getRole());
+        return request;
+    }
+}
