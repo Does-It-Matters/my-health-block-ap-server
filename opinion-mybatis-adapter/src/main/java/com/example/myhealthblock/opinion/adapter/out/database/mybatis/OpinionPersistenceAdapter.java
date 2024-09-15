08979a84b4102bc2fc491c123faf43ee47a68163
glass <silver20190204@gmail.com>
2024-09-16 00:27:02 +0900

    opinion mybatis 어댑터 분리 (Issue #139)

--- opinion-mybatis-adapter/src/main/java/com/example/myhealthblock/opinion/adapter/out/database/mybatis/OpinionPersistenceAdapter.java
+++ opinion-mybatis-adapter/src/main/java/com/example/myhealthblock/opinion/adapter/out/database/mybatis/OpinionPersistenceAdapter.java
@@ -1,7 +1,5 @@
 package com.example.myhealthblock.opinion.adapter.out.database.mybatis;
 
-import com.example.myhealthblock.aop.LogExecutionTime;
-import com.example.myhealthblock.aop.LogTarget;
 import com.example.myhealthblock.opinion.application.port.out.OpinionOutputPort;
 import com.example.myhealthblock.opinion.application.port.out.dto.OpinionEnrollOutputPortRequest;
 import com.example.myhealthblock.opinion.application.port.out.dto.OpinionOutputPortDTO;
@@ -9,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-@LogExecutionTime(logTarget = LogTarget.ADAPTER)
+//@LogExecutionTime(logTarget = LogTarget.ADAPTER)
 @RequiredArgsConstructor
 public class OpinionPersistenceAdapter implements OpinionOutputPort {
     private final OpinionMapper opinionMapper;
