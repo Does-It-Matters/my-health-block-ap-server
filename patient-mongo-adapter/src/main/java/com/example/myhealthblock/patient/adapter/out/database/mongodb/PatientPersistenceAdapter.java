ae65dd9d004819ba25bd84c6606507c37c587ec2
glass <silver20190204@gmail.com>
2024-09-16 00:48:53 +0900

    patient mongo 어댑터 분리 (Issue #139)

--- patient-mongo-adapter/src/main/java/com/example/myhealthblock/patient/adapter/out/database/mongodb/PatientPersistenceAdapter.java
+++ patient-mongo-adapter/src/main/java/com/example/myhealthblock/patient/adapter/out/database/mongodb/PatientPersistenceAdapter.java
@@ -1,11 +1,9 @@
 package com.example.myhealthblock.patient.adapter.out.database.mongodb;
 
-import com.example.myhealthblock.aop.LogExecutionTime;
-import com.example.myhealthblock.aop.LogTarget;
 import com.example.myhealthblock.patient.application.port.out.PatientOutputPort;
 import lombok.RequiredArgsConstructor;
 
-@LogExecutionTime(logTarget = LogTarget.ADAPTER)
+//@LogExecutionTime(logTarget = LogTarget.ADAPTER)
 @RequiredArgsConstructor
 public class PatientPersistenceAdapter implements PatientOutputPort {
     private final PatientRepository patientRepository;
